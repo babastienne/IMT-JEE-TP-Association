8c494fd787164b3ab0464fe8eb45485a982bc505
dahmaniselim <selim.dahmani@forcity.io>
2017-10-31 14:02:17 +0100

    add: Hello World!

---
+++ src/main/java/Hello.java
@@ -0,0 +1,33 @@
+import javax.jws.WebService;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class Hello extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+        process(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException
+    {
+        process(request, response);
+    }
+
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello world ! </h1>");
+        out.println("<h1>getServletName() : " +
+                getServletName() + "</h1>");
+
+        out.println("<h1>URI : " +
+
+                request.getRequestURI() + "</h1>");
+    }
+}
