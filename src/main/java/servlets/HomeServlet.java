795b70208398c1a860600116d1f4c6c6ced08631
Quentin Eude <quentineude@gmail.com>
2017-11-03 21:21:54 +0100

    Add home servlet

---
+++ src/main/java/servlets/HomeServlet.java
@@ -0,0 +1,26 @@
+package servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "HomeServlet", urlPatterns = {"/index"})
+public class HomeServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        process(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        process(request,response);
+    }
+
+    private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String destination = "index.jsp";
+        RequestDispatcher rd = request.getRequestDispatcher(destination);
+        rd.forward(request,response);
+    }
+}
