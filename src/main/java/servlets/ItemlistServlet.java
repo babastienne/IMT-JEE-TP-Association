d3a97d529254b460d64d507d48f4bb4f3d3f96e1
Quentin Eude <quentineude@gmail.com>
2017-11-05 14:11:51 +0100

    Refactoring

--- src/main/java/servlets/ItemlistServlet.java
+++ src/main/java/servlets/ItemlistServlet.java
@@ -20,41 +20,31 @@ import java.util.List;
 
 import static database.Entity.ENTITY;
 
-@WebServlet(name = "ItemlistServlet", urlPatterns = {"/itemlist","/itemlist/add"})
+@WebServlet(name = "ItemlistServlet", urlPatterns = {"/itemlist"})
 public class ItemlistServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        process(request,response);
+        TokenChecker.checkConnection(request,response);
+        EntityManager em= ENTITY.getEntity();
+        String code = request.getParameter("code");
+        String quantity = request.getParameter("quantity");
+        Item item = em.find(Item.class, Long.parseLong(code));
+        item.setStock(item.getStock() - Integer.parseInt(quantity));
+        ENTITY.update(item);
+        response.sendRedirect("/itemlist");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        process(request, response);
-    }
-
-    private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         TokenChecker.checkConnection(request,response);
-        String path = request.getServletPath();
         EntityManager em= ENTITY.getEntity();
-        switch (path){
-            case "/itemlist":
-                CriteriaBuilder cb = em.getCriteriaBuilder();
-                CriteriaQuery<Item> c = cb.createQuery(Item.class);
-                Root<Item> e = c.from(Item.class);
-                c.select(e);
-                Query query = em.createQuery(c);
-                List<Item> listItems = (List<Item>) query.getResultList();
-                request.setAttribute("listItems", listItems);
-                String destination = "itemlist.jsp";
-                RequestDispatcher rd = request.getRequestDispatcher(destination);
-                rd.forward(request,response);
-                break;
-            case "/itemlist/add":
-                String code = request.getParameter("code");
-                String quantity = request.getParameter("quantity");
-                Item item = em.find(Item.class, Long.parseLong(code));
-                item.setStock(item.getStock() - Integer.parseInt(quantity));
-                ENTITY.update(item);
-                break;
-        }
-
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Item> c = cb.createQuery(Item.class);
+        Root<Item> e = c.from(Item.class);
+        c.select(e);
+        Query query = em.createQuery(c);
+        List<Item> listItems = (List<Item>) query.getResultList();
+        request.setAttribute("listItems", listItems);
+        String destination = "itemlist.jsp";
+        RequestDispatcher rd = request.getRequestDispatcher(destination);
+        rd.forward(request,response);
     }
 }
