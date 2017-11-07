71aa4933ca5093b9014e67eeb604ebb900c0df71
maveist <maveist44@gmail.com>
2017-11-07 22:04:25 +0100

    add some correction

--- src/main/java/servlets/ItemlistServlet.java
+++ src/main/java/servlets/ItemlistServlet.java
@@ -1,5 +1,6 @@
 package servlets;
 
+import models.Authentification.AuthManager;
 import models.Item;
 import models.OrderLine;
 import models.ServiceOrder;
@@ -28,72 +29,44 @@ import static database.Entity.ENTITY;
 @WebServlet(name = "ItemlistServlet", urlPatterns = {"/itemlist"})
 public class ItemlistServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        TokenChecker.checkConnection(request,response);
-        if(!response.isCommitted()) {
+        TokenChecker.checkConnection(request, response);
+        if (!response.isCommitted()) {
             EntityManager em = ENTITY.getEntity();
 
-        // Ajout d'article dans la commande
-        String code = request.getParameter("code");
-        String quantity = request.getParameter("quantity");
-        Item item = em.find(Item.class, Long.parseLong(code));
-        item.setStock(item.getStock() - Integer.parseInt(quantity));
-        ENTITY.update(item);
+            // Ajout d'article dans la commande
+            String code = request.getParameter("code");
+            String quantity = request.getParameter("quantity");
+            Item item = em.find(Item.class, Long.parseLong(code));
+            item.setStock(item.getStock() - Integer.parseInt(quantity));
+            ENTITY.update(item);
 
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<ServiceUser> c = cb.createQuery(ServiceUser.class);
+            String authSession = (String) request.getSession().getAttribute("authToken");
+            ServiceUser user = em.find(ServiceUser.class, AuthManager.getUID(authSession));
+            ServiceOrder serviceOrder = user.getOrder();
 
-
-
-
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<ServiceUser> c = cb.createQuery(ServiceUser.class);
-        Root<ServiceUser> e = c.from(ServiceUser.class);
-        String authCookie = "";
-        Cookie[] cookies = request.getCookies();
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("authToken")) {
-                authCookie = cookie.getValue();
+            OrderLine ol = null;
+            boolean isAlreadyIn = false;
+            for (OrderLine i : serviceOrder.getOrders()) {
+                if (i.getItem().getId() == item.getId()) {
+                    isAlreadyIn = true;
+                    ol = i;
+                    break;
+                }
             }
-        }
-        c.select(e).where(cb.equal(e.get("id"), AuthManager.getUID(authCookie)));
-        Query query = em.createQuery(c);
-        ServiceUser user = (ServiceUser) query.getResultList().get(0);
 
-        CriteriaBuilder cb2 = em.getCriteriaBuilder();
-        CriteriaQuery<ServiceOrder> c2 = cb2.createQuery(ServiceOrder.class);
-        Root<ServiceOrder> e2 = c2.from(ServiceOrder.class);
-        c2.select(e2).where(cb2.equal(e2.get("user"), user.getUid()));
-        Query query2 = em.createQuery(c2);
-        List<ServiceOrder> so = (List<ServiceOrder>) query2.getResultList();
-
-        ServiceOrder serviceOrder;
-        if(so != null && !so.isEmpty()){
-            serviceOrder = so.get(0);
-        }else{
-            serviceOrder = new ServiceOrder(user);
-            ENTITY.create(serviceOrder);
-        }
-
-        OrderLine ol = null;
-        boolean isAlreadyIn = false;
-        for(OrderLine i : serviceOrder.getOrders()){
-            if(i.getItem().getId() == item.getId()){
-                isAlreadyIn = true;
-                ol = i;
-                break;
+            if (isAlreadyIn) {
+                ol.setQuantityItem(ol.getQuantityItem() + Integer.parseInt(quantity));
+                ENTITY.update(ol);
+            } else {
+                ol = new OrderLine(item, serviceOrder, Integer.parseInt(quantity));
+                ENTITY.create(ol);
             }
+
+
+            response.sendRedirect("/itemlist");
         }
-
-        if(isAlreadyIn){
-            ol.setQuantityItem(ol.getQuantityItem() + Integer.parseInt(quantity));
-            ENTITY.update(ol);
-        }else{
-            ol = new OrderLine(item, serviceOrder, Integer.parseInt(quantity));
-            ENTITY.create(ol);
-        }
-
-
-
-
-        response.sendRedirect("/itemlist");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
