8698d3cc3293df43ed2a5e9a559169a1260d5fac
Babastienne <potiron.bastien@gmail.com>
2017-11-07 21:07:32 +0100

    remove unused imports

--- src/main/java/servlets/AuthServlet.java
+++ src/main/java/servlets/AuthServlet.java
@@ -1,11 +1,16 @@
 package servlets;
 
-import models.Authentification.AuthManager;
+import java.io.IOException;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
-import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import models.Authentification.AuthManager;
 
 
 /**
--- src/main/java/servlets/RegisterServlet.java
+++ src/main/java/servlets/RegisterServlet.java
@@ -7,11 +7,9 @@ import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import models.ServiceUser;
 import models.Authentification.AuthUser;
